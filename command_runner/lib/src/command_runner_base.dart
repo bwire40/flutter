2b3aa79c21374f0bcbf3c2c5c8451d2c736a23bc
bwire40 <ebwire10@proton.me>
2026-02-10 07:57:07 +0300

    updates on packages

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -1,6 +1,9 @@
-// TODO: Put public facing types in this file.
-
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class CommandRunner {
+  /// Runs the command-line application logic with the given arguments.
+  // ignore: unintended_html_in_doc_comment
+  /// Future<void> is a return type that indicates that
+  /// this method might perform asynchronous operations, but doesn't return a value.
+  Future<void> run(List<String> input) async {
+    print('CommandRunner received arguments: $input');
+  }
 }
